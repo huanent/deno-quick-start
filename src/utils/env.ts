6dd611d3f1a074931a11ea4af2297915dabb3b84
huanent <huanent@icloud.com>
2022-11-23 20:47:19 +0800

    Add dotenv

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,6 @@
+import { config } from "dotenv";
 import { dirname } from "path";
 
+await config({ export: true });
 export const isDev = Deno.env.get("DENO_ENV") == "development";
 export const basePath = isDev ? Deno.cwd() : dirname(Deno.execPath());
